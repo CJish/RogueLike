b106e958b3436f5617be7e929f02d265e002a1bf
CJish <christopheru82@gmail.com>
2024-03-07 21:45:21 -0800

    Step04 complete, a player moves around the world

--- src/Main/Models/World.java
+++ src/Main/Models/World.java
@@ -22,6 +22,24 @@ public class World {
         }
     }
 
+    public void dig(int x, int y) {
+        if(tile(x,y).isDiggable()) {tiles[x][y] = Tile.FLOOR;}
+    }
+
+    public void addAtEmptyLocation(Creature creature) {
+        int x;
+        int y;
+
+        do {
+            x = (int)(Math.random() * width);
+            y = (int)(Math.random() * height);
+        }
+        while (!tile(x,y).isGround());
+
+        creature.x = x;
+        creature.y = y;
+    }
+
     public char glyph(int x, int y) {
         return tile(x, y).glyph();
     }
