e4af52c2dcea80a9ee849f013f9cbc0b8e6ca07c
CJish <christopheru82@gmail.com>
2024-03-07 21:09:52 -0800

    Step03 complete, world populates and can scroll

---
+++ src/Main/Models/World.java
@@ -0,0 +1,37 @@
+package Main.Models;
+
+import java.awt.*;
+
+public class World {
+
+    private Tile[][] tiles;
+    private int width;
+    private int height;
+
+    public World(Tile[][] tiles) {
+        this.tiles = tiles;
+        this.width = tiles.length;
+        this.height = tiles[0].length;
+    }
+
+    public Tile tile(int x, int y) {
+        if (x < 0 || x >= width || y < 0 || y >= height) {
+            return Tile.BOUNDS;
+        } else {
+            return tiles[x][y];
+        }
+    }
+
+    public char glyph(int x, int y) {
+        return tile(x, y).glyph();
+    }
+
+    public Color color(int x, int y) {
+        return tile(x, y).color();
+    }
+
+    // interesting take on Getters
+    public int width() {return width;}
+    public int height() {return height;}
+
+}
